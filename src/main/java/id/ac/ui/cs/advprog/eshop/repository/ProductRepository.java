0deb4ae05f5a1f30bc2ce5675df80eb676b48f4d
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-02-06 16:56:53 +0700

    add delete product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,4 +19,19 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product deleteById(String productId) {
+        Product productToDelete = findById(productId);
+        productData.remove(productToDelete);
+        return productToDelete;
+    }
 }
