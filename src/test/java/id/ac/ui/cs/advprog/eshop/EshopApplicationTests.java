264e3f51129c9d8a7f99af3db2e740526d0f2a86
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-02-14 15:52:39 +0700

    add assertions in eshop app unit test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,14 +3,18 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class EshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNotNull(EshopApplication.class);
 	}
 	@Test
 	void mainMethodStartsApplication() {
 		EshopApplication.main(new String[]{});
+		assertNotNull(EshopApplication.class);
 	}
 }
