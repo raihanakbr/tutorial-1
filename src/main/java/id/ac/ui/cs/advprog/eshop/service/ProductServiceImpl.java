cf2501e797c2b981af9d066f6ec32febb148b621
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-02-14 15:46:58 +0700

    fix field injection changed to constructor injection in ProductServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -12,8 +12,11 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService{
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
     public Product create(Product product) {
