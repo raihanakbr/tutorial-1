0f7e627d9d2ecb20323273f0b7423b3ddfdc4fb4
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-02-14 15:34:57 +0700

    fix homepage method name consistency issue

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomePageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomePageController {
 
     @GetMapping("")
-    public String HomePage() {
+    public String homePage() {
         return "homePage";
     }
 }
