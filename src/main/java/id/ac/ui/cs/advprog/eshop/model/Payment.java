51256cdc73dfc0734357e5499465e3a386326f26
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-03-06 19:49:36 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
 import lombok.Getter;
 
 import java.util.Map;
-import java.util.List;
-import java.util.Arrays;
 
 @Getter
 public class Payment {
@@ -23,26 +24,32 @@ public class Payment {
     }
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData) {
-        this(id, method, order, paymentData, "PENDING");
+        this(id, method, order, paymentData, PaymentStatus.PENDING.getValue());
     }
 
     private void setOrder(Order order) {
         if (order == null) {
             throw new IllegalArgumentException("Order cannot be null");
         }
+
         this.order = order;
     }
 
     public void setStatus(String status) {
-        List<String> statusList = Arrays.asList("PENDING", "SUCCESS", "REJECTED");
-
-        if (!statusList.contains(status)) {
+        if (!PaymentStatus.contains(status)) {
             throw new IllegalArgumentException("Invalid payment status");
         }
+
         this.status = status;
     }
 
     protected void setPaymentData(Map<String, String> paymentData) {
+        if (PaymentMethod.contains(this.method)) {
+            throw new IllegalArgumentException(
+                    "Unable to assign payment data specific to a method when the payment method is not specified"
+            );
+        }
+
         this.paymentData = null;
     }
 }
