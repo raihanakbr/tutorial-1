dbe78fbb12315450b14fe1b5d403f194eec6b808
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-03-06 20:44:07 +0700

    [GREEN] Implement PaymentBankTransfer model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentBankTransfer.java
@@ -3,16 +3,21 @@ package id.ac.ui.cs.advprog.eshop.model;
 import java.util.Map;
 
 public class PaymentBankTransfer extends Payment {
-
-    public PaymentBankTransfer(String id, String method, Order order, Map<String, String> paymentData, String status) {
+    public PaymentBankTransfer(String id, Order order, String method, Map<String, String> paymentData, String status) {
         super(id, method, order, paymentData, status);
     }
 
-    public PaymentBankTransfer(String id, String method, Order order, Map<String, String> paymentData) {
+    public PaymentBankTransfer(String id, Order order, String method, Map<String, String >paymentData) {
         super(id, method, order, paymentData);
     }
 
     @Override
     protected void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData.get("bankName").isBlank() ||
+                paymentData.get("referenceCode").isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.paymentData = paymentData;
     }
 }
