7cda11a2e8764b415e1016df9a061e54780491b4
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-03-06 20:34:30 +0700

    [REFACTOR] Remove @builder from Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,12 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import java.util.List;
-
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
-import lombok.Builder;
+
 import lombok.Getter;
 
-@Builder
+import java.util.List;
+
 @Getter
 public class Order {
     String id;
@@ -15,13 +14,13 @@ public class Order {
     String author;
     String status;
 
-    public Order(String id, List<Product> products, Long orderTime, String author){
+    public Order(String id, List<Product> products, Long orderTime, String author) {
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
         this.status = OrderStatus.WAITING_PAYMENT.getValue();
 
-        if(products.isEmpty()) {
+        if (products.isEmpty()) {
             throw new IllegalArgumentException();
         } else {
             this.products = products;
